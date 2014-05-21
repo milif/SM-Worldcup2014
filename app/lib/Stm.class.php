d509878f906dfc739c8d222c00c3f9eafb6945eb
Михаил <milif777@gmail.com>
2014-05-21 16:25:50 +0400

    feat(openid): авторизация через сотмаркет

--- app/lib/Stm.class.php
+++ app/lib/Stm.class.php
@@ -4,10 +4,26 @@ class Stm {
     const API_URL = 'http://www.sotmarket.ru';
     static public function auth(){
         if($_GET['stm'] == "1") {
-            self::__post(self::API_URL, array(''));
+            return self::__userData(
+                self::__post(self::API_URL, array(
+                    'cookie' => $_COOKIE['sotmarketid']
+                ))
+            );
+            
         }
     }
-    static public function __post($url, $params){
+    static private function __userData($data){
+        if($data['errcode'] !== 0) return false;
+        return array(
+            'uri' => "stm:".$data['id'],
+            'email' => $data['email'],
+            'avatar' => NULL,
+            'dob' => isset($data['birthday']) ? $data['birthday'] : NULL,
+            'name' => $data['full_name'],
+            'gender' => NULL
+        );
+    }
+    static private function __post($url, $params){
         $options = array(
             'http' => array(
                 'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
@@ -16,6 +32,7 @@ class Stm {
             ),
         );
         $context  = stream_context_create($options);
-        $accessData = json_decode(file_get_contents($url, false, $context), true);
+        $jsonData = json_decode(file_get_contents($url, false, $context), true);
+        return $jsonData;
     }
 }
