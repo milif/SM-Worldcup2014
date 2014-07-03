112dd274d86c64300faee2b152cc9523fd88fc0b
Михаил <milif777@gmail.com>
2014-07-03 12:03:21 +0400

    feat(app): промокод

--- app/db/reload_bets.php
+++ app/db/reload_bets.php
@@ -9,4 +9,4 @@ DB::update("UPDATE user_bets SET score = 0, result = null;");
 require __DIR__.'/update_bets.php';
 
 // Начисляем баллы по промокодам
-DB::update("UPDATE user a, (SELECT user_id, SUM(score) score FROM code GROUP BY user_id) b SET a.score = a.score + b.score WHERE a.id = b.user_id");
+DB::update("UPDATE user a, (SELECT user_id, SUM(score) score FROM code WHERE used_at IS NOT NULL GROUP BY user_id) b SET a.score = a.score + b.score WHERE a.id = b.user_id");
